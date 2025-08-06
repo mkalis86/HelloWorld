9bd7d8e41a2fd8c7a9e4c15dd6d0527fa044f25d
mkalis86 <mkalis86@gmail.com>
2025-08-06 08:57:56 +0300

    Create happ.py

---
+++ happ.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Hello World", layout="wide")
+
+st.title("👋 Hello, Streamlit on Android!")
+st.write("Αυτό είναι ένα demo Streamlit app που μπορεί να εγκατασταθεί σαν εφαρμογή στο Android σου (PWA).")
